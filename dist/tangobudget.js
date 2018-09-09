946e6da1857fbeff78996a37a82835210c3b9d44
Agustina <agustina.risso25@gmail.com>
2018-09-08 21:42:11 -0300

    Cambia panel resultados y pdf

--- dist/tangobudget.js
+++ dist/tangobudget.js
@@ -655,7 +655,7 @@ function toggleBounce(){
           // Si da el tiempo, ver como hacer que robote cada vez que se hace click en el marcador (sin arrastrar)
         }
 function printPDF(){
-       var doc = new jsPDF();          
+  /*     var doc = new jsPDF();          
     var elementHandler = {
       '#ignorePDF': function (element, renderer) {
         return true;
@@ -670,7 +670,24 @@ function printPDF(){
           'width': 180,'elementHandlers': elementHandler
         });
     
-    doc.output("dataurlnewwindow");
+    doc.output("dataurlnewwindow");*/
+
+  var pdf = new jsPDF('p', 'pt', 'letter'); //lo genera vacío! Y da errores raros
+    pdf.canvas.height = 72 * 11;
+    pdf.canvas.width = 72 * 8.5;
+
+    pdf.fromHTML(document.body);
+
+    pdf.save('test.pdf');
+
+ /* var printDoc = new jsPDF();
+    printDoc.fromHTML($('#pdf').get(0), 10, 10, {'width': 180});
+    printDoc.autoPrint();
+    printDoc.output("dataurlnewwindow"); //Este abre una pagina aparte en el browser pero vacía*/
+
+    // this opens a new popup,  after this the PDF opens the print window view but there are browser inconsistencies with how this is handled
+
+
 }
 // Convert from radians to degrees.
 function toDegrees(radians){
--- src/PrintPDF.js
+++ src/PrintPDF.js
@@ -1,5 +1,5 @@
 function printPDF(){
-       var doc = new jsPDF();          
+  /*     var doc = new jsPDF();          
     var elementHandler = {
       '#ignorePDF': function (element, renderer) {
         return true;
@@ -14,5 +14,22 @@ function printPDF(){
           'width': 180,'elementHandlers': elementHandler
         });
     
-    doc.output("dataurlnewwindow");
+    doc.output("dataurlnewwindow");*/
+
+  var pdf = new jsPDF('p', 'pt', 'letter'); //lo genera vacío! Y da errores raros
+    pdf.canvas.height = 72 * 11;
+    pdf.canvas.width = 72 * 8.5;
+
+    pdf.fromHTML(document.body);
+
+    pdf.save('test.pdf');
+
+ /* var printDoc = new jsPDF();
+    printDoc.fromHTML($('#pdf').get(0), 10, 10, {'width': 180});
+    printDoc.autoPrint();
+    printDoc.output("dataurlnewwindow"); //Este abre una pagina aparte en el browser pero vacía*/
+
+    // this opens a new popup,  after this the PDF opens the print window view but there are browser inconsistencies with how this is handled
+
+
 }
