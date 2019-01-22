7d4c3745cc6674a8a0613772b9c8bdaeb29f5e5b
Agustina <agustina.risso25@gmail.com>
2019-01-21 22:44:58 -0300

    Nueva modificación de Channel Availability 2

--- src/calculations/ChannelAvailability2.js
+++ src/calculations/ChannelAvailability2.js
@@ -1,13 +1,15 @@
-function DisponibilidadCanal (distancia, MargenFading) {
+function DisponibilidadCanal (distancia, MargenFading, htx, hrx) {
     
     var dN1 = -400;
     var rugosidad;
+    var alturaantena;
     
-    var htx; //hay que importar los valores de htx, hrx y frecuencia
-    var hrx;
-    var frec;
-    var alturaantena; // aqui se debe guardar la altura de la antena más baja
-    
+    if (htx<hrx){
+        alturaantena = htx; // aqui se debe guardar la altura de la antena más baja
+    }
+    else 
+        alturaantena = hrx;
+
     var A = document.getElementById("FactorRugosidad").value;
     var arrayA= [0, 4, 1, 0.25];
 
@@ -23,8 +25,9 @@ function DisponibilidadCanal (distancia, MargenFading) {
 var k = Math.pow(10, -4.4-0.0027*dN1)*Math.pow(10 + rugosidad, -0.46);
 var epsilon = Math.abs(htx-hrx)/distancia;
 
-var Pw = k*Math.pow(1 + epsilon, -1.03)*Math.pow(frec, 0.8)*Math.pow(10, -0.00076*alturaantena*MargenFading/10);
+var Pw = k*Math.pow(1 + epsilon, -1.03)*Math.pow(Inputfreq, 0.8)*Math.pow(10, -0.00076*alturaantena*MargenFading/10);
+var dispanual = 1- Pw;
 
-return Pw;
+return dispanual;
 
 }
