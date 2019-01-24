46bdb3e4129f33b471afaa05b1b4791e74e37061
Claudia <claudia.duarte@correo.ucu.edu.uy>
2019-01-23 21:31:33 -0300

    Channel availability2 actualizado con htx, hrx e inputfreq correcto

--- dist/tangobudget.js
+++ dist/tangobudget.js
@@ -180,15 +180,12 @@ function DispCanal(distancia,MargenFading) {
 }
 
 function DisponibilidadCanal (distancia, MargenFading) {
-    
+
     var dN1 = -400;
     var rugosidad;
-    
-    var htx; //hay que importar los valores de htx, hrx y frecuencia
-    var hrx;
-    var frec;
+
     var alturaantena; // aqui se debe guardar la altura de la antena más baja
-    
+
     var A = document.getElementById("FactorRugosidad").value;
     var arrayA= [0, 4, 1, 0.25];
 
@@ -199,16 +196,17 @@ function DisponibilidadCanal (distancia, MargenFading) {
 	else {
 		rugosidad = arrayA [A];
 	    }
-    
+
 
 var k = Math.pow(10, -4.4-0.0027*dN1)*Math.pow(10 + rugosidad, -0.46);
-var epsilon = Math.abs(htx-hrx)/distancia;
+var epsilon = Math.abs(altura[0]-altura[cant_redondeo-1])/distancia;
 
-var Pw = k*Math.pow(1 + epsilon, -1.03)*Math.pow(frec, 0.8)*Math.pow(10, -0.00076*alturaantena*MargenFading/10);
+var Pw = k*Math.pow(1 + epsilon, -1.03)*Math.pow(Inputfreq, 0.8)*Math.pow(10, -0.00076*alturaantena*MargenFading/10);
 
 return Pw;
 
 }
+
 function Fresnel(Pmax,h_Pmax){
   var lambda;
   if(!Inputfreq){
