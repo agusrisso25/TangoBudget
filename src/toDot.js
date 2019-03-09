07cef81347ac38868969209a8750140c81fb1df8
Claudia <claudia.duarte@correo.ucu.edu.uy>
2019-03-09 18:00:52 -0300

    toDot

--- src/toDot.js
+++ src/toDot.js
@@ -1,15 +1,21 @@
 /* Esta función convierte la coma a punto y el punto al punto.
 */
 function parseNumber(numberString){
-  /*var reMatch = /^[+-]?(\d+)(?:[,.](\d+))?$/.exec(numberString);
-  if (!reMatch) {
-    return NaN;
-  } else if (!reMatch[2]) {
-    return +reMatch[1];
-  } else {
-    return +(reMatch[1] +'.'+ reMatch[2]);
-  }*/
-  var res = numberString.replace(",", ".");
+  var busqueda=numberString.search(",");
+  var res;
+  if (busqueda>=0)
+    res = numberString.replace(",", ".");
+  else {
+    res= parseFloat(numberString);//numberString.toString().replace(/[.]/, ",");
+    /*var reMatch = /^[+-]?(\d+)(?:[,.](\d+))?$/.exec(numberString);
+    if (!reMatch) {
+      return NaN;
+    } else if (!reMatch[2]) {
+      return +reMatch[1];
+    } else {
+      return +(reMatch[1] +'.'+ reMatch[2]);
+    }*/
+  }
   res=parseFloat(res);
   return (res);
 }
